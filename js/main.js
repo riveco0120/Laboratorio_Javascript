4ef3099ffb60383176f1d7e83fb0af3ee42bc44b
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-21 13:34:26 -0500

    Finalizando video 2

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
         this.width=width;
         this.height = height;
         this.palying = false;
-        this.game_over=false;
+        this.gameOver=false;
         this.bars = [];
         this.ball = null;
     }
@@ -13,7 +13,7 @@
     self.Board.prototype={
         get elements(){
             var elements =this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
@@ -40,7 +40,7 @@
     }
 
     //Funciones para mover las barras
-    self.Board.prototype={
+    self.Bar.prototype={
         down:function(){
 
         },
@@ -56,39 +56,42 @@
 
 //Clase vista
 (function(){
-    self.BoardView = function(canvas,board){
+    self.BoardView = function(canvas,Board){
         this.canvas =canvas;
-        this.canvas.width = board.width;
-        this.canvas.height= board.height;
-        this.borad = board;
+        this.canvas.width = Board.width;
+        this.canvas.height= Board.height;
+        this.Borad = Board;
         this.contexto = canvas.getContext("2d");
 
 
 }
 
-self.BoardView.prototype={
+   self.BoardView.prototype ={
     draw:function(){
-        for(var i = board.elements.length-1;i>=0;i--){
-            var elementoDibujado =board.elements[i];
-            draw(this.contexto,elementoDibujado);
-        }
-
+        for (var i = this.board.elements.lenght-1; i>=0; i--) {
+            var elementoDibujado = this.board.elements[i];
+          
+            draw(this.contexto,elementoDibujado)
+        };
     }
 }
 
 
 //Dibujando los elemensto
 function draw(contexto,element){
-    switch ((element.kind)) {
-        case "Squeare":
-            //Funcion para dibujar el cuadrado con las cordenadas, altura y anchura
-            contexto.fillReact(element.x,element.y,element.width,element.height);
-           break;
-    
-        default:
-            break;
+    if(element!==null && element.hasOwnProperty("kind")){
+        switch ((element.kind)) {
+            case "rectangle":
+                //Funcion para dibujar el cuadrado con las cordenadas, altura y anchura
+                contexto.fillReact(element.x,element.y,element.width,element.height);
+               break;
+        
+               default:
+                break;
+        }
+        
     }
-}
+    }
 })();
 
 window.addEventListener("load",main)
@@ -98,8 +101,12 @@ function main(){
 
     //instanciar objetos
     var board = new Board(800,400);
+    var bar = new Bar(20,100,40,100,board);
+    var barDos = new Bar(700,100,40,100,board);
     var canvas = document.getElementById('canvas');
     //Pasando el model a la vista
     var board_view = new BoardView(canvas,board);
+   //Dibujando todos los elementos 
+    board_view.draw();
 
 }
